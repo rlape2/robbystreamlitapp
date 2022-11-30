457c4ae8840387aae47a2fc5b7cbf771cef12ee0
RobbyLape <robby.lape@gmail.com>
2022-11-30 14:55:40 -0500

    commit

--- pages/1_🌍_TN_Land_Use.py
+++ pages/1_🌍_TN_Land_Use.py
@@ -1,30 +1,8 @@
 import streamlit as st
-import leafmap.foliumap as leafmap
+import geemap.foliumap as geemap
+import ee 
 
-markdown = """
-Web App URL: <https://template.streamlit.app>
-GitHub Repository: <https://github.com/giswqs/streamlit-multipage-template>
-"""
-
-st.sidebar.title("About")
-st.sidebar.info(markdown)
-logo = "https://i.imgur.com/UbOXYAU.png"
-st.sidebar.image(logo)
+Map = geemap.Map()
+Map.to_streamlit()
 
 
-st.title("Interactive Map")
-
-col1, col2 = st.columns([4, 1])
-options = list(leafmap.basemaps.keys())
-index = options.index("OpenTopoMap")
-
-with col2:
-
-    basemap = st.selectbox("Select a basemap:", options, index)
-
-
-with col1:
-
-    m = leafmap.Map(locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
-    m.add_basemap(basemap)
-    m.to_streamlit(height=700)
