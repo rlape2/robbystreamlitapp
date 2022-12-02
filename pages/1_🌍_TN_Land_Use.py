35d6bba541db7cc5219d9b96444e0d27e65fbbd2
Robby Lape <rlape@vols.utk.edu>
2022-12-02 11:17:46 -0500

    f

--- pages/1_🌍_TN_Land_Use.py
+++ pages/1_🌍_TN_Land_Use.py
@@ -6,7 +6,7 @@ st.title("TN Land Use")
 
  
 
-col1, col2 = st.columns([7, 3])
+col1, col2 = st.columns([2, 1])
 
 counties = ee.FeatureCollection('TIGER/2018/Counties')
 feat = counties.filter(ee.Filter.eq('STATEFP', '47'))
