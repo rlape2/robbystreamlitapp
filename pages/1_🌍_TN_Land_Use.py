faa55425c7e735285fc975514b7c137c329f403b
Robby Lape <rlape@vols.utk.edu>
2022-12-02 10:56:11 -0500

    fix

--- pages/1_🌍_TN_Land_Use.py
+++ pages/1_🌍_TN_Land_Use.py
@@ -34,9 +34,10 @@ Map = geemap.Map()
 
 dataset = ee.Image('USGS/NLCD_RELEASES/2019_REL/NLCD/2019')
 landcover = dataset.select('landcover')
-Map.addLayer(landcover, {}, 'NLCD 2019')
-
-fc = feat.filter(ee.Filter.eq('NAME', county))
+# Map.addLayer(landcover, {}, 'NLCD 2019')
+TN = landcover.clipToCollection(feat)
+Map.addLayer(TN, {}, 'NLCD 2019')
+fc = feat.filter(ee.Filter.eq('NAMELSAD', county))
 dataset = dataset.clipToCollection(fc)
 Map.centerObject(fc)
 
