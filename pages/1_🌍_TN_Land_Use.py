235cd2bd0b3b1ad883a6da9c955ecaebd27cd935
Robby Lape <rlape@vols.utk.edu>
2022-12-02 10:26:05 -0500

    County LandUse

--- pages/1_🌍_TN_Land_Use.py
+++ pages/1_🌍_TN_Land_Use.py
@@ -4,7 +4,47 @@ import ee
 
 st.title("TN Land Use")
 
+ 
+
+col1, col1 = st.columns([4, 1])
+
+counties = ee.FeatureCollection('TIGER/2018/Counties')
+feat = counties.filter(ee.Filter.eq('STATEFP', '47'))
+style = {'fillColor': '00000000'}
+
+options = feat.aggregate_array('NAMELSAD').getInfo()
+options.sort()
+
+default_county = "Knox County"
+
+options.remove(default_county)
+options.insert(0, default_county)
+options.insert(1, "Williamson County")
+options.insert(2, "Maury County")
+
+default_index = options.index("Knox County")
+
+with col2:
+
+    county = st.selectbox("Select a county", options, default_index )
+
+
+
 Map = geemap.Map()
-Map.to_streamlit()
+
+dataset = ee.Image('USGS/NLCD_RELEASES/2019_REL/NLCD/2019')
+landcover = dataset.select('landcover')
+Map.addLayer(landcover, {}, 'NLCD 2019')
+
+fc = feat.filter(ee.Filter.eq('NAMELSAD', county))
+ls = dataset.clipToCollection(fc)
+map.centerObject(fc)
+
+
+Map.addLayer(feat.style(**style), {}, "Counties", False)
+
+
+with col1:
+    Map.to_streamlit()
 
 
