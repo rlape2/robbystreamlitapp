30a6e696510cd8312b379244e844c0735305bb50
RobbyLape <robby.lape@gmail.com>
2023-05-07 13:40:30 -0400

    big change

--- pages/demo.py
+++ pages/demo.py
@@ -6,12 +6,18 @@ st.set_page_config(layout = "wide")
 
 col1, col2 = st.columns([4, 1])
 
+options = list(leafmap.basemaps.keys())
+
 with col2:
     dropdown = st.selectbox("Basemap", ['HYBRID', 'TERRAIN', 'SATELLITE'])
+    url = st.text_input("Enter URL")
 
 m = leafmap.Map()
 m.add_basemap("HYBRID")
 m.add_basemap(dropdown)
+
+if url:
+    m.add_tile_layer(url, name='Tile Layer', attribution=' ')
 
 with col1:
     m.to_streamlit()
