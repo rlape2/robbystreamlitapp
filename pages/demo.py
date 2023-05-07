14a279544bcdb67a850c425513e450fe71d2efa4
RobbyLape <robby.lape@gmail.com>
2023-05-07 13:20:37 -0400

    change

--- pages/demo.py
+++ pages/demo.py
@@ -4,9 +4,11 @@ import geemap.foliumap as leafmap
 
 st.set_page_config(layout = "wide")
 
+dropdown = st.selectbox("Basemap", ['HYBRID', 'TERRAIN', 'SATELLITE'])
+
 m = leafmap.Map()
 m.add_basemap("HYBRID")
 m.to_streamlit()
 
-dropdown = st.selectbox("Basemap", ['HYBRID', 'TERRAIN', 'SATELLITE'])
+#dropdown = st.selectbox("Basemap", ['HYBRID', 'TERRAIN', 'SATELLITE'])
 m.add_basemap(dropdown)
